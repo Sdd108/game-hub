e64602b641a1e7bb26f595c3d64bb5eabb447f2f
“Zhipeng <zhipengmail@qq.com>
2025-12-18 22:30:07 +0800

    use ColorModeButton

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,12 @@
-import {
-  HStack,
-  Image,
-  ClientOnly,
-  IconButton,
-  Skeleton,
-} from "@chakra-ui/react";
-import { useColorMode } from "@/components/ui/color-mode";
-import { LuMoon, LuSun } from "react-icons/lu";
+import { HStack, Image } from "@chakra-ui/react";
+import { ColorModeButton } from "@/components/ui/color-mode";
 import logo from "@/assets/logo.webp";
-import {} from "@chakra-ui/react";
 
 const NavBar = () => {
-  const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      <ClientOnly fallback={<Skeleton boxSize="8" />}>
-        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
-          {colorMode === "light" ? <LuSun /> : <LuMoon />}
-        </IconButton>
-      </ClientOnly>
+      <ColorModeButton />
     </HStack>
   );
 };
