5bc4b8b92c9ab02680206ec417bb6a998001341a
“Zhipeng <zhipengmail@qq.com>
2025-12-19 00:26:06 +0800

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe, BsNintendoSwitch } from "react-icons/bs";
+import type { Platform } from "@/hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: BsNintendoSwitch,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color="gray.500"
+          boxSize={5}
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
