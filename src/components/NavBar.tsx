60ea8be28782d9dcc3d0a42141ac8f0446d36543
“Zhipeng <zhipengmail@qq.com>
2025-12-18 22:24:25 +0800

    Add the colorMode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,25 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  ClientOnly,
+  IconButton,
+  Skeleton,
+} from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import { LuMoon, LuSun } from "react-icons/lu";
 import logo from "@/assets/logo.webp";
+import {} from "@chakra-ui/react";
 
 const NavBar = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+          {colorMode === "light" ? <LuSun /> : <LuMoon />}
+        </IconButton>
+      </ClientOnly>
     </HStack>
   );
 };
