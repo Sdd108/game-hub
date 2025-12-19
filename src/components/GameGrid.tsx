71fe0db1953d077e966801295a9aea26132f3afb
“Zhipeng <zhipengmail@qq.com>
2025-12-19 21:32:27 +0800

    Displaying critic score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5, "2xl": 6 }}
         padding="10px"
         gap="40px"
       >
--- src/components/theme.ts
+++
@@ -1,5 +0,0 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-
-const config: ThemeConfig ={
-  initialColorMode: "dark"
-}
