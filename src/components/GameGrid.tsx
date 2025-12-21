1f8a5fe7f815fdec4dfe8ea6c04b70b2831969ef
“Zhipeng <zhipengmail@qq.com>
2025-12-21 17:42:50 +0800

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,9 +17,9 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 3, "2xl": 6 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 6 }}
         padding="10px"
-        gap={4}
+        gap={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
