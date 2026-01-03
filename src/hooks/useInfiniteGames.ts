a053bda3c44fbcd44845c36e64d4427e036091bb
“Zhipeng <zhipengmail@qq.com>
2026-01-03 22:01:16 +0800

    Fetch game details

--- src/hooks/useInfiniteGames.ts
+++ src/hooks/useInfiniteGames.ts
@@ -7,6 +7,8 @@ import APIClient, { type FetchResponse } from "@/services/api-client";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
